0c941fed1143d259f03a397174fd23410b51d8c2
ostapchikk <ostap-974@mail.ru>
2023-05-16 12:34:58 +0300

    refactor

---
+++ js/modules/timer.js
@@ -0,0 +1,30 @@
+function Timer(){
+
+   const hours = document.querySelector('#hours');
+   const minutes = document.querySelector('#minutes');
+   const seconds = document.querySelector('#seconds');
+   
+   const nextTime = Date.parse(new Date()) + 10000000;
+   
+   function updateCounter() {
+      const currentTime = new Date();
+      const diff = nextTime - currentTime;
+   
+      // Перевод в дни
+      const daysLeft = Math.floor(diff / 1000 / 60 / 60 / 24);
+      // Часов всего, далее остаток от деления на 24 (преобразования в дни), получаем 8 часов
+      const hoursLeft = Math.floor(diff / 1000 / 60 / 60) % 24;
+      // Минут всего, далее остаток от преобразования в часы, минут осталось
+      const minutesLeft = Math.floor(diff / 1000 / 60) % 60;
+      // Секундк всего, далее остаток от преобразования в минуты, секунд осталось
+      const secondsLeft = Math.floor(diff / 1000) % 60;
+   
+      hours.innerText = hoursLeft < 10 ? '0' + hoursLeft : hoursLeft;
+      minutes.innerText = minutesLeft < 10 ? '0' + minutesLeft : minutesLeft;
+      seconds.innerText = secondsLeft < 10 ? '0' + secondsLeft : secondsLeft;
+   }
+   
+   setInterval(updateCounter, 1000);
+}
+
+export default Timer;
